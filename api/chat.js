b7d942e06299b009035683afc39d6cb81fccf1cb
Ironprincess <ovetta@ovetta-sampson.com>
2026-03-31 12:23:24 -0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -179,7 +179,7 @@ async function logToUpstash(question) {
   });
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
